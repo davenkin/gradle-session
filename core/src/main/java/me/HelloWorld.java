858eeabc71827c93aac1c2d70e208b4658950939
Teng Yun <davenkin@gmail.com>
2015-09-22 17:58:27 +0800

    dd

---
+++ core/src/main/java/me/HelloWorld.java
@@ -0,0 +1,10 @@
+package me;
+
+/**
+ * Created by yteng on 9/22/15.
+ */
+public class HelloWorld {
+    public  String hello() {
+        return "Hello World!";
+    }
+}
---
+++ web/src/main/java/me/Hello.java
@@ -0,0 +1,25 @@
+package me;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by yteng on 9/22/15.
+ */
+public class Hello extends HttpServlet {
+
+    public void doGet(HttpServletRequest req,
+                      HttpServletResponse res)
+            throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+
+        out.println(new HelloWorld().hello());
+        out.close();
+    }
+}
+
