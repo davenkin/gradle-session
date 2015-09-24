425f8b48c89dbace524f14e086eadb20fc82c9ec
Teng Yun <davenkin@gmail.com>
2015-09-24 10:51:08 +0800

    Teng Yun:added hello servlet

--- web/src/main/java/helloworld/servlet/HelloWorldServlet.java
+++ web/src/main/java/helloworld/servlet/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package helloworld.servlet;
 
 import helloworld.HelloWorld;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
